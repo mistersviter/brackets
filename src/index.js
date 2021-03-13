a84ae9391cd4eed8efe8c1703cc3d5d714217faf
Tarabrin Evgeniy <mistersviter@gmail.com>
2021-03-13 04:42:30 +0400

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let newBracketsConfig = [];
+    let arrStr = '';
+    let matchFound = 0;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        arrStr = '';
+        for (let j = 0; j < bracketsConfig[i].length; j++) {
+            arrStr += bracketsConfig[i][j];
+        }
+        newBracketsConfig.push(arrStr);
+    }
+    do {
+        matchFound = 0;
+        newBracketsConfig.forEach(function(element) {
+            if (str.includes(element)) {
+                matchFound++;
+                str = str.replace(element, '')
+            }
+        });
+    } while (matchFound && str.length);
+    if (str !== '') {
+        return false;
+    } else {
+        return true;
+    }
 }
